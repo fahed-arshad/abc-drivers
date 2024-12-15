53e2f9fe0768b0b98f6777ceee2b506c390f973d
TharindaPrabhath <tharindahp@gmail.com>
2024-12-15 16:29:15 +0530

    fix dashboard 404 issue

--- src/app/[locale]/auth/verify/[[...verify]]/page.tsx
+++ src/app/[locale]/auth/verify/[[...verify]]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 
-import { useRouter } from 'next/navigation';
+import { useRouter } from '@/i18n/routing';
 
 import z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
