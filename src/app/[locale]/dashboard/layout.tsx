a6a7eb73f81f44e3eb023beae2b07a3ec8f67305
TharindaPrabhath <tharindahp@gmail.com>
2024-12-15 15:45:57 +0530

    minor changes

--- src/app/[locale]/dashboard/layout.tsx
+++ src/app/[locale]/dashboard/layout.tsx
@@ -15,7 +15,7 @@ async function DashboardLayout({
       <Header locale={locale} className="fixed z-[999] top-0 left-0 right-0" />
       <div className="sm:container flex flex-row absolute top-28 left-0 right-0 overflow-hidden h-[calc(100vh-10.5rem)]">
         <Sidebar />
-        <main className="flex-1 overflow-y-auto">{children}</main>
+        <main className="flex-grow overflow-y-auto">{children}</main>
       </div>
       <Footer className="absolute bottom-0 left-0 right-0" />
     </div>
